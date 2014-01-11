50455d5b320076b60a1649296f8327ba66fecedf
kraftman <crtanner@gmail.com>
2014-01-11 13:32:28 +0000

    intial commit 2

---
+++ src/main/java/es/themin/empires/empires.java
@@ -0,0 +1,30 @@
+package es.themin.empires;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public final class empires extends JavaPlugin {
+ 
+	
+	@Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+		getLogger().info("onEnable has been invoked!");
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    	if(cmd.getName().equalsIgnoreCase("basic")){ // If the player typed /basic then do the following...
+    		// doSomething
+    		getLogger().info("your crap plugin works!");
+    		return true;
+    	} //If this has happened the function will return true. 
+            // If this hasn't happened the a value of false will be returned.
+    	return false; 
+    }
+}
