8dacf18b2edb45d2587ce5c7b5141c7a8ccedaf4
ben <b.jenner2@hotmail.com>
2014-01-14 19:04:18 +0000

    Pushing core

--- src/main/java/es/themin/empires/util/Core.java
+++ src/main/java/es/themin/empires/util/Core.java
@@ -1,12 +1,17 @@
 package es.themin.empires.util;
 
+import org.bukkit.Location;
+
 public class Core {
 	
+	private enum CoreType { BASIC, MOB, FARM, MONSTER, GRIEF, FORTIFICATION;}
 	private int Id;
 	private CoreType type;
-	public Core(int Id, CoreType type) {
+	private Location location;
+	public Core(int Id, CoreType type, Location location) {
 		this.Id = Id;
 		this.type = type;
+		this.location = location;
 	}
 	public int getId(){
 		return Id;
@@ -14,6 +19,19 @@ public class Core {
 	public CoreType getType(){
 		return type;
 	}
-	
+	public Location getLocation(){
+		return location;
+	}
+	public Location[] getFlagSlots() {
+		CoreType type = getType();
+		if (type == CoreType.BASIC) {
+			Location[] slots = {new Location(location.getWorld(), location.getBlockX() + 1, location.getBlockY() + 2, location.getBlockZ()),new Location(location.getWorld(), location.getBlockX() - 1, location.getBlockY() + 2, location.getBlockZ()),new Location(location.getWorld(), location.getBlockX(), location.getBlockY() + 2, location.getBlockZ() + 1), new Location(location.getWorld(), location.getBlockX(), location.getBlockY() + 2, location.getBlockZ()+ 1)  };
+			return slots;
+		}
+		else {
+			return null;
+		}
+		
+	}
 
 }
