677442c2e40b0a85d249603ec3ab048844e5d8c6
ben <b.jenner2@hotmail.com>
2014-01-17 23:39:01 +0000

    Some new events

--- src/main/java/es/themin/empires/events/CoreDamageEvent.java
+++ src/main/java/es/themin/empires/events/CoreDamageEvent.java
@@ -1,5 +1,58 @@
 package es.themin.empires.events;
 
-public class CoreDamageEvent {
+
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import enums.CoreDamageType;
+import es.themin.empires.util.Core;
+
+public class CoreDamageEvent extends Event implements Cancellable{
+	
+	private CoreDamageType damagetype;
+	private Core core;
+	private Block block;
+	private Player player;
+	public boolean cancelled;
+	private static final HandlerList handlers = new HandlerList();
+	public CoreDamageEvent(Block block, Core core, CoreDamageType coredamager, Player player) {
+		this.block = block;
+		this.core = core;
+		this.damagetype = coredamager;
+		this.player = player;
+	}
+    public boolean isCancelled() {
+        return cancelled;
+    }
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
+    }
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    public Block getBlock(){
+    	return block;
+    }
+    public Core getCore(){
+    	return core;
+    }
+    public CoreDamageType getCoreDamager(){
+    	return this.damagetype;
+    }
+    public Player getPlayer(){
+    	return player;
+    }
+    public boolean isPlayerBreaking(){
+    	if (damagetype == CoreDamageType.PLAYER) return true;
+    	else return false;
+    }
+	
 
 }
--- src/main/java/es/themin/empires/events/CoreUpgradeEvent.java
+++ src/main/java/es/themin/empires/events/CoreUpgradeEvent.java
@@ -1,5 +1,39 @@
 package es.themin.empires.events;
 
-public class CoreUpgradeEvent {
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import enums.CoreUpgradeType;
+import es.themin.empires.util.Core;
+
+public class CoreUpgradeEvent extends Event implements Cancellable{
+	
+	private static final HandlerList handlers = new HandlerList();
+	public boolean cancelled;
+	private Core core;
+	private CoreUpgradeType cut;
+	public CoreUpgradeEvent(Core core, CoreUpgradeType cut) {
+		this.core = core;
+		this.cut = cut;
+	}
+    public boolean isCancelled() {
+        return cancelled;
+    }
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
+    }
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    public Core getCore(){
+    	return this.core;
+    }
+    public CoreUpgradeType getUpgradeType(){
+    	return this.cut;
+    }
 
 }
