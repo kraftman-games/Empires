2a92925ee5de65f422b88df6123928de7f735c35
ben <b.jenner2@hotmail.com>
2014-01-18 12:56:01 +0000

    Changed type again

--- src/main/java/es/themin/empires/util/testing/UtilityTesting.java
+++ src/main/java/es/themin/empires/util/testing/UtilityTesting.java
@@ -19,8 +19,7 @@ import es.themin.empires.util.UtilManager;
 
 public class UtilityTesting implements CommandExecutor{
 	
-	private empires plugin;
-	private String plprefix = plugin.plprefix;
+	private String plprefix = empires.plprefix;
 	private static ArrayList<SubCommand> commands = new ArrayList<SubCommand>();
 	public static void setUp(){
 		commands.add(new newemp());
