31481b65a6db7e8f8bdc82f160a0b6b16d87f54a
kraftman <crtanner@gmail.com>
2014-01-18 12:47:12 +0000

    first test of core block protection

--- src/main/java/es/themin/empires/Listeners/BlockListener.java
+++ src/main/java/es/themin/empires/Listeners/BlockListener.java
@@ -1,9 +1,15 @@
 package es.themin.empires.Listeners;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 
 import es.themin.empires.empires;
 
@@ -19,7 +25,14 @@ public class BlockListener implements Listener {
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event){
 		Block myBlock = event.getBlock();
-		
+		List<MetadataValue> values = myBlock.getMetadata("coreType");
+		for(MetadataValue value : values){
+			if (value.getOwningPlugin().equals(plugin)){
+				String myCoreType = value.asString();
+				Bukkit.broadcastMessage("cannot delete core block of type: " + myCoreType);
+				event.setCancelled(true);
+			}
+		}
 		
 	}
 }
