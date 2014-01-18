f2d6a930454311ce0ec218646da16a69893daf71
kraftman <crtanner@gmail.com>
2014-01-18 12:29:22 +0000

    Commit test

--- src/main/java/es/themin/empires/Listeners/BlockListener.java
+++ src/main/java/es/themin/empires/Listeners/BlockListener.java
@@ -1,6 +1,9 @@
 package es.themin.empires.Listeners;
 
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 import es.themin.empires.empires;
 
@@ -10,5 +13,13 @@ public class BlockListener implements Listener {
 	private empires plugin;
 	public BlockListener(empires myPlugin){
 		this.plugin = myPlugin;
+	}
+	
+	
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event){
+		Block myBlock = event.getBlock();
+		
+		
 	}
 }
