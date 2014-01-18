1b7fea61fdbf28c9c6a5007295f36a093211c14c
kraftman <crtanner@gmail.com>
2014-01-18 23:39:52 +0000

    test charging shards to delete base core

--- src/main/java/es/themin/empires/Listeners/BlockListener.java
+++ src/main/java/es/themin/empires/Listeners/BlockListener.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
 
@@ -37,8 +41,19 @@ public class BlockListener implements Listener {
 		
 		if (myCore != null){
 			if (myCore.getEmpire() == UtilManager.empireplayers.get(player.getName())){
-				Bukkit.broadcastMessage("deleted core block of type: " + myCoreType);
-				myCore.Delete();
+				Inventory i = player.getInventory();
+		          for(ItemStack inven : i.getContents()){
+		              if(inven.getType().equals(Material.FLINT)){
+		            	  ItemMeta myMeta = inven.getItemMeta();
+		            	  if (myMeta.getDisplayName().equals("Core Shard")){
+		            		  Bukkit.broadcastMessage("deleted core block of type: " + myCoreType);
+		            		  player.getInventory().remove(inven);
+		      				 myCore.Delete();
+		      				 break;
+		            	  }
+		              }
+		          }
+				
 			}else {
 				Bukkit.broadcastMessage("cannot delete core block of type: " + myCoreType);
 				event.setCancelled(true);
