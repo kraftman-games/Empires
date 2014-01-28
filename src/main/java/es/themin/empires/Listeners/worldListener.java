fb0de828954223a9f9f00ff2d11346a6cfb97716
kraftman <crtanner@gmail.com>
2014-01-28 23:40:33 +0000

    It works! I'll leave the timer there in case we decide to do something snazzier later

--- src/main/java/es/themin/empires/Listeners/worldListener.java
+++ src/main/java/es/themin/empires/Listeners/worldListener.java
@@ -20,13 +20,7 @@ public class worldListener implements Listener{
 	
 	@EventHandler
 	public void onChunkPopulate(ChunkPopulateEvent event){
-		//if (event.isNewChunk()){
-			Player myPlayer = Bukkit.getServer().getPlayer("kraftman");
-			if (UtilManager.tannerTemp != null){
-				//myPlayer.teleport(UtilManager.tannerTemp);
-				//UtilManager.tannerTemp = null;
-			}
-		//}
+		
 		
 	}
 }
