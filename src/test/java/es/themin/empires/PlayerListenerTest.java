9a0bfedf7b053ba267370f0e2ca0d19e4b85e6a2
kraftman <crtanner@gmail.com>
2014-01-29 22:28:30 +0000

    added some playerlistener unit tests

---
+++ src/test/java/es/themin/empires/PlayerListenerTest.java
@@ -0,0 +1,29 @@
+package es.themin.empires;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerListenerTest {
+
+	
+	
+	@Before
+	public void setupEnvironment(){
+		
+	}
+	
+	@Test
+	public void newPlayerJoin(){
+		//do we create an empire
+		//do we generate a base core
+		
+	
+	}
+	
+	@Test void returningPlayerJoin(){
+		
+		
+	}
+	
+	
+}
