252f115cce22d8c073944d1ae38354846bc71dee
ben <b.jenner2@hotmail.com>
2014-02-15 10:17:01 +0000

    tt

--- src/main/java/es/themin/empires/Listeners/Event_BlockPlace.java
+++ src/main/java/es/themin/empires/Listeners/Event_BlockPlace.java
@@ -37,7 +37,7 @@ public class Event_BlockPlace implements Listener{
 		ItemMeta im = event.getItemInHand().getItemMeta();
 		Material placed = event.getBlockPlaced().getType();
 		if (placed == Material.IRON_FENCE) {
-			if (im.getDisplayName().contains("Pop Up Jail")) {
+			if (im.getDisplayName().contains("base core")) {
 				player.sendMessage("ItemCorrect");
 				if (myCorePlayer == null) {
 					player.sendMessage(MsgManager.notinemp);
