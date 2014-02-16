79d02882245524c2e74da97ab31c057c019143fb
kraftman <crtanner@gmail.com>
2014-02-16 23:34:41 +0000

    bh

--- src/main/java/es/themin/empires/EmpiresDAL.java
+++ src/main/java/es/themin/empires/EmpiresDAL.java
@@ -76,7 +76,7 @@ public class EmpiresDAL {
 			
 			if (connection != null){
 			
-		        PreparedStatement stmnt = connection.prepareStatement("SELECT * FROM `Players` where `UUID` = ?;");
+		        PreparedStatement stmnt = connection.prepareStatement("SELECT * FROM `Players` where `UUID` = ?");
 		        stmnt.setString(1, myPlayer.getUniqueId().toString());
 		        System.out.println(stmnt.toString());
 		        //myPlayer.sendMessage(stmnt.toString());
