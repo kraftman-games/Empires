c418c0a35af24105c120246a3f79e494b1c1923d
kraftman <crtanner@gmail.com>
2014-02-25 21:00:10 +0000

    trying to fix the build offset

--- src/main/java/es/themin/empires/cores/CoreFactory.java
+++ src/main/java/es/themin/empires/cores/CoreFactory.java
@@ -7,17 +7,25 @@ import org.bukkit.Location;
 import es.themin.empires.cores.coretypes.BaseCore;
 import es.themin.empires.cores.coretypes.GriefCore;
 import es.themin.empires.enums.CoreType;
+import es.themin.empires.util.testing.newemp;
 
 public class CoreFactory {
 
 	
 	public static Core CreateCore(UUID empireUUID, Location myLocation, CoreType myCoreType){
 		
+		Integer xInteger = myLocation.getBlockX();
+		Integer yInteger = myLocation.getBlockY();
+		Integer zInteger = myLocation.getBlockZ();
+		
+		Location myLocation2 = new Location(myLocation.getWorld(), xInteger, yInteger, zInteger);
+		
+		
 		switch (myCoreType) {
 		case BASE:
-			return new BaseCore( empireUUID,  myLocation);
+			return new BaseCore( empireUUID,  myLocation2);
 		case GRIEF:
-			return new GriefCore( empireUUID,  myLocation);
+			return new GriefCore( empireUUID,  myLocation2);
 		default:
 			return null;
 		}
