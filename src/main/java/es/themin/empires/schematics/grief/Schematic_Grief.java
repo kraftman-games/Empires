14ac2b03b68e7a86cd8bf3fbfbcca186815f3bde
ben <b.jenner2@hotmail.com>
2014-02-25 21:14:16 +0000

    tt

---
+++ src/main/java/es/themin/empires/schematics/grief/Schematic_Grief.java
@@ -0,0 +1,44 @@
+package es.themin.empires.schematics.grief;
+
+import java.util.ArrayList;
+
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import es.themin.empires.enums.CoreType;
+import es.themin.empires.schematics.Schematic;
+
+public class Schematic_Grief extends Schematic{
+
+	@Override
+	public CoreType coreType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Effect getEffectType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void pasteFromCentre(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Block> getBlocks(Location location) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
